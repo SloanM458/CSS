ad3a1cb65c75a9a1fc001e200598f1bb82abedee
Sloan Molstad <sloan.molstad@gmail.com>
2023-01-27 11:20:05 -0800

    update

---
+++ Algos/W1D4.js
@@ -0,0 +1,46 @@
+// var fruit1 = "apples";
+// var fruit2 = "oranges";
+    
+// fruit2 = fruit1;
+    
+// console.log(fruit2 + " and " + fruit1);
+
+// var arr = ['apples', 'oranges', 'lemons', 'kiwi', 'mango','longan']
+
+// console.log(arr1[3])
+
+// var temp= arr1[3] //holding onto the value of 'kiwi'
+
+// arr1[1] = 'blueberries'
+
+// arr1[3]=arr1[1]
+// // 'kiwi' = 'blueberries', where kiwi was, were gonna turn it into blueberries
+
+// console.log(arr[1])
+
+// var poppedFruit = arr1.pop();
+// console.log(poppedFruit);
+
+----------------------------------------------
+
+
+//
+// we want to reverse this
+function reverseArray(arr){
+arr1=["a", "b", "c", "d", "e"]
+    for(var i =4; i>)
+temp=[]
+}
+
+
+
+function reverseArray(arr){
+    for(var left= 0; left<arr.length/2; left++) {
+        var right= arr1.length-1-left;
+        if (arr1[left] != arr[right]) {
+        }
+    }
+}
+
+//we want to add on for an array
+

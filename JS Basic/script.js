b3a630d5e49dcce2823229d78a17debd54d5acbd
Sloan Molstad <sloan.molstad@gmail.com>
2023-01-30 14:13:33 -0800

    update

--- JS Basic/script.js
+++ JS Basic/script.js
@@ -1,18 +1,33 @@
-console.log('Hello from the console!');
 
-function alertmsg(message) {
-    alert(message)
+
+var pokePic= document.querySelector('#pokemon');
+var pokeName = document.querySelector('#pokemonName');
+var pokeTeam = document.querySelector('#pokeTeam');
+
+function evolvePokemon(){
+    document.querySelector('#pokemon').src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwq4UOLnEqEVDPhzLZ6seR9_KHoec9V3tiwA&usqp=CAU'
+    // Here, we put the id or class in (' '), same selectors from css apply to query
+    //the .src allows us to go past the this element to change the image element
+
+    pokeName.innerText = 'Carracosta'
 }
 
-function thisEl(element){
-    console.log(element.innerText);
-}
-
-function turnOff(element) {
-    element.innerText = "Off";
-}
 
 
-function changeImg(element){
-    element.src= 'https://assets.pokemon.com/assets/cms2/img/misc/countries/au/country_detail_pokemon.png'
-}
+
+// function alertmsg(message) {
+//     alert(message)
+// }
+
+// function thisEl(element){
+//     console.log(element.innerText);
+// }
+
+// function turnOff(element) {
+//     element.innerText = "Off";
+// }
+
+
+// function changeImg(element){
+//     element.src= 'https://assets.pokemon.com/assets/cms2/img/misc/countries/au/country_detail_pokemon.png'
+// }
